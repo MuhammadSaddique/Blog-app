94af2785b3a3a019a5cd21347871f81e6fa822bc
MuhammadSaddique <saddiqueshah588@gmail.com>
2024-01-05 05:23:20 +0500

    Privte Post

--- dashbord.js
+++ dashbord.js
@@ -35,11 +35,16 @@ var db = getFirestore(app);
 // console.log(db , "db is working");
 
 var parent = document.getElementById("parent");
+var exampleModal1 = document.getElementById("exampleModal1");
+
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal1'), {
+  keyboard: false
+})
 
 window.addEventListener("load",async function () {
-  console.log("blog load");
+  // console.log("blog load");
   var uid = localStorage.getItem("uid");
-  console.log(uid, "uid");
+  // console.log(uid, "uid");
 
   if (!uid) {
     location.replace("./index.html");
@@ -52,7 +57,7 @@ window.addEventListener("load",async function () {
       console.log(doc.data());
       console.log(doc.id);
 
-      BlogArr.push(doc.data())
+      // BlogArr.push(doc.data())
 
       BlogArr.push({
         title:doc.data().title,
@@ -60,6 +65,7 @@ window.addEventListener("load",async function () {
         uid: doc.data().uid,
         image: doc.data().image,
         blogId: doc.id,
+        isPrivate: doc.data().isPrivate,
       })
 
   })
@@ -67,12 +73,28 @@ window.addEventListener("load",async function () {
 
 
   for(var value of BlogArr){ 
-    parent.innerHTML += renderCardUI(
-      value.title,
-       value.desc,
-       value.image,
-       value.blogId
-     );
+    if(value.isPrivate){
+
+      if(value.uid === uid){
+        parent.innerHTML += renderCardUI(
+          value.title,
+           value.desc,
+           value.image,
+           value.blogId,
+           value.isPrivate,
+         );
+      }
+      
+    }else{
+      parent.innerHTML += renderCardUI(
+        value.title,
+         value.desc,
+         value.image,
+         value.blogId,
+         value.isPrivate,
+       );
+     
+    }
    
   }
 
@@ -81,32 +103,47 @@ window.addEventListener("load",async function () {
 
 async function createBlog(){
     console.log(createBlog,);
-    var title = document.getElementById("title").value;
-    var desc = document.getElementById("desc").value;
+    var title = document.getElementById("title");
+    var desc = document.getElementById("desc");
     var uid = localStorage.getItem("uid");
+  var privatePost = document.getElementById("privatePost").checked;
+
+
 
     var blogObj = {
-        title: title,
-        desc: desc,
+        title: title.value,
+        desc: desc.value,
         uid: uid,
         image: "",
+        isPrivate: privatePost,
     }
 
     const docRef = await addDoc(collection(db,"blogs"),blogObj);
-    parent.innerHTML = renderCardUI(title.value,desc.value," ", docRef.id);
+    parent.innerHTML += renderCardUI(title.value, desc.value, "", docRef.id,privatePost);
+    myModal.hide();
     
-    title.value = "";
-    desc.value = "";
+    title.value = ""
+    desc.value = ""
     console.log(docRef,"docRef");
 }
 
 
-function renderCardUI(title,desc,image,id){
+function renderCardUI(title,desc,image,id,isPrivate){
+  // console.log("UI is Private",isPrivate);
+  var lockValue = "";
+  if(isPrivate){
+    lockValue = `<i class="fa fa-lock" aria-hidden="true"></i>`;
+  }else{
+    lockValue = "";
+  }
+
+
+
   var UI = `
   <div class="card" style="width: 18rem;">
         <img src="https://picsum.photos/300/200" class="card-img-top" alt="">
         <div class="card-body">
-          <h5 class="card-title">${title}</h5>
+          <h5 class="card-title">${title} ${lockValue} </h5>
           <p class="card-text">
           ${desc}
           </p>

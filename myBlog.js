ba1452bac82b1b2a100bd5c484fa2e4b02ebad4d
MuhammadSaddique <saddiqueshah588@gmail.com>
2024-01-07 05:00:20 +0500

    blogs changes

--- myBlog.js
+++ myBlog.js
@@ -1,1 +1,13 @@
-console.log("");
+console.log("myblog Page");
+
+
+window.addEventListener("load", function () {
+    console.log("blog load");
+    var uid = localStorage.getItem("uid");
+    console.log(uid, "uid");
+  
+    if (!uid) {
+      location.replace("./index.html");
+      return;
+    }
+  });
